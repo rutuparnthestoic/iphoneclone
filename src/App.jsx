cdc4bf83ab881441283612c63637e44c956782bf
Rutuparn Kakade <134830170+rutuparnthestoic@users.noreply.github.com>
2024-05-12 20:20:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Model from './components/Model';
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
 import Footer from "./components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const App = () => {
   return (
     <main className="bg-black">
+    <SpeedInsights />
     <Navbar />
     <Hero />
     <Highlights />
