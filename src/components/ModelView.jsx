aa52eeb41da3351cdd1a57646a78ba2a5c080041
Rutuparn Kakade <134830170+rutuparnthestoic@users.noreply.github.com>
2024-05-12 20:02:41 +0530

    Add files via upload

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,46 @@
+import {Html,  OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
+import Lights from "./Lights"
+import { Suspense } from "react"
+import IPhone from './IPhone'
+import * as THREE from 'three'
+import Loader from "./Loader"
+
+const ModelView = ({index, groupRef, gsapType, controlRef, setRotationState, size, item}) => {
+  return (
+    <View
+    index = {index}
+    id={gsapType}
+    className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+    <ambientLight intensity={3}>
+    <PerspectiveCamera makeDefault position={[0,0,4]}/>
+    <Lights />
+
+    <OrbitControls 
+    ref={controlRef}
+    enableZoom={false}
+    enablePan={false}
+    rotateSpeed={0.4}
+    target={new THREE.Vector3(0,0,0)}
+    onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+
+    />
+
+    <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0,0,0]}>
+
+    </group>
+
+
+    <Suspense fallback={<Loader />}>
+    <IPhone 
+      scale={index === 1 ? [15,15,15] : [17,17,17]}
+      item={item}
+      size={size}
+    />
+    </Suspense>
+    </ambientLight>
+    </View>
+  )
+}
+
+export default ModelView
